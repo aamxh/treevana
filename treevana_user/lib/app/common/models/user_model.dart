5fd3b21228b7a5a7e496f2683ac6822748e13758
aamxh <a.a.mxhamed@gmail.com>
2025-11-11 14:50:54 +0100

    added missed user_model.dart

---
+++ treevana_user/lib/app/common/models/user_model.dart
@@ -0,0 +1,26 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user_model.g.dart';
+
+@JsonSerializable()
+class UserModel {
+
+  final String name;
+  final String email;
+  final String password;
+  final String phoneNumber;
+  final String picture;
+
+  UserModel({
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.phoneNumber,
+    required this.picture,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) =>
+      _$UserModelFromJson(json);
+  Map<String, dynamic> toJson() => _$UserModelToJson(this);
+
+}
---
+++ treevana_user/lib/app/common/models/user_model.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserModel _$UserModelFromJson(Map<String, dynamic> json) => UserModel(
+  name: json['name'] as String,
+  email: json['email'] as String,
+  password: json['password'] as String,
+  phoneNumber: json['phoneNumber'] as String,
+  picture: json['picture'] as String,
+);
+
+Map<String, dynamic> _$UserModelToJson(UserModel instance) => <String, dynamic>{
+  'name': instance.name,
+  'email': instance.email,
+  'password': instance.password,
+  'phoneNumber': instance.phoneNumber,
+  'picture': instance.picture,
+};
